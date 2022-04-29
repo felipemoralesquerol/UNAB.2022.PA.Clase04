66641ef16cbf275de01c688f64473bccf9658d15
Felipe Morales <felipe.morales.querol@gmail.com>
2022-04-29 20:28:46 -0300

    Fix metaclase2 y metaclase3

--- metaclase3.py
+++ metaclase3.py
@@ -1,15 +1,21 @@
+# Ejemplos de herencia múltiple
 Animal = type('Animal', (), {'nombre':'Animal a definir'})
 
+Vegetal = type('Vegental', (), {'nombrev':'Vegetal a definir'})
 
 def dice(self):
     print('Guauuuuuu')
 
 
 Perro = type('Perro', (Animal, ),dict(cantidad_patas=4, dice=dice))
-
+Hatana = type('Hatana', (Animal, Vegetal, ), dict())
 
 print('Perro')
 p = Perro()
 print(p.nombre)          # Imprime a definir
 print(p.cantidad_patas)  # Imprime 4
 p.dice()                 # Imprime Guauuuuuu
+
+print('Hatana')
+h = Hatana()
+print(h.nombre)
