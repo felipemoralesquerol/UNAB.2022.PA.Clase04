34f922d3999fd6a37e5219657f66c164f7ddc368
Felipe Morales <felipe.morales.querol@gmail.com>
2022-05-09 21:51:17 -0300

    Fix menor

--- metaclase3.py
+++ metaclase3.py
@@ -1,23 +1,26 @@
 # Ejemplos de herencia múltiple
 Animal = type('Animal', (), {'nombre':'Animal a definir'})
 
-Vegetal = type('Vegental', (), {'nombrev':'Vegetal a definir'})
+Vegetal = type('Vegetal', (), {'nombre_veg':'Vegetal a definir'})
 
 def dice(self):
     print('Guauuuuuu!')
 
 
 Perro = type('Perro', (Animal, ),dict(cantidad_patas=4, dice=dice))
-Hatana = type('Hatana', (Animal, Vegetal, ), dict())
+
+
+Hatana = type('Hatana', (Vegetal,Animal, ), dict())
 
 # Operaciones con clase Perro
-print('Perro')
+print('Operaciones sobre Perro')
 p = Perro()
 print(p.nombre)          # Imprime a definir
 print(p.cantidad_patas)  # Imprime 4
 p.dice()                 # Imprime Guauuuuuu
-
+print()
 # Operaciones con clase Hatana
-print('Hatana')
+print('Operaciones sobre Hatana')
 h = Hatana()
 print(h.nombre)
+print(h.nombre_veg)
