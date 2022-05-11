ecf8e38ca0a3b448e68370fd93ed29835d160842
Felipe Morales <felipe.morales.querol@gmail.com>
2022-05-11 20:48:26 -0300

    Fix decorators

--- decoradores2.py
+++ decoradores2.py
@@ -1,5 +1,6 @@
+# Función decoradora
 def calcular_area_triangulo(function):
-
+    # Función de retorno
     def funcion_de_retorno(*args, **kwargs):
         #print(*args)
         res = function(*args, **kwargs)
@@ -7,11 +8,11 @@ def calcular_area_triangulo(function):
 
     return funcion_de_retorno
 
-
+# Uso de decorador
 @calcular_area_triangulo
 def calcular_area(base, altura):
     return base * altura
 
-
+# Invocación
 if __name__ == "__main__":
     print(calcular_area(4, 10))
